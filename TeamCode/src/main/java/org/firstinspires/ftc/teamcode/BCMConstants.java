28f6a5bb1b725de72583dc465898f1cd77a891ad
Pablo Cafiso <pcypc@hotmail.com>
2023-11-02 23:19:55 -0400

    Added and started coding a constants class to hold all required conts values in one place.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BCMConstants.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+public final class BCMConstants {
+    public static final class AnalogInputConstants {
+        public static final double kVoltageJoystickAtRest = 1.0;
+        public static final double kVoltageJoystickEngaged = 0.5;
+
+        //Analog Inputs names
+        public static final String kJoystickUp = "JoystickUp"; //Analog Input port #0
+    }
+}
